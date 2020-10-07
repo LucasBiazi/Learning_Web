927a7fd9a42f6664e1d6073fa83e476daecc3314
LucasBiazi <lucas.ferreira@novaandradina.org>
2020-10-07 13:14:12 -0400

    Difference among var, let and const.

---
+++ JavaScript_studies/JS_Basics/Variables.js
@@ -0,0 +1,24 @@
+// There are 3 manners to declare a variable in js: var, let and const.
+
+// Var variables can be used globally
+var global = "Global Variable";
+
+function variable_global() {
+  console.log("I can be used in here! I'm a " + global);
+}
+variable_global();
+
+// Let variables are the ones that may have its values reassigned
+let i = 0;
+for (i; i < 10; i++) console.log("Let is cool");
+console.log("Value of i after the for loop: " + i);
+
+// Const variables are the ones that will not have its values reassigned
+const name = "Lucas Biazi Ferreira";
+function my_quote() {
+  console.log(
+    "'You can always change the value of the variables,\nhowever, by using let/const, your code is easier to read!' -" +
+      name
+  );
+}
+my_quote();
