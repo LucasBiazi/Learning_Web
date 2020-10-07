d898564c431e8c692e7cf0fc52dd8c482692fe88
LucasBiazi <lucas.ferreira@novaandradina.org>
2020-10-07 13:33:33 -0400

    Basic operators in js.

---
+++ JavaScript_studies/JS_Basics/Basic_math.js
@@ -0,0 +1,30 @@
+let result = 0;
+let n1 = 10;
+let n2 = -5;
+console.log("n1 = " + n1);
+console.log("n2 = " + n2);
+console.log("\n");
+
+// Addition
+result = n1 + n2;
+console.log("Addition: " + result);
+
+// Subtraction
+result = n1 + n2;
+console.log("Subtraction: " + result);
+
+// Multiplication
+result = n1 * n2;
+console.log("Multiplication: " + result);
+
+// Division
+result = n1 / n2;
+console.log("Division: " + result);
+
+// Raised to
+result = n1 ** 3;
+console.log("N1 raised to 3: " + result);
+
+// Rest of division
+result = n1%n2
+console.log("N1%N2 = " + result)
