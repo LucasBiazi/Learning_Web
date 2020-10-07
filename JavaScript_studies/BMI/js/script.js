aa0b8c7d0a0f2fd83d2697f482945183de5a3e35
LucasBiazi <lucas.ferreira@novaandradina.org>
2020-10-07 00:57:12 -0400

    Little updates.

--- JavaScript_studies/BMI/js/script.js
+++ JavaScript_studies/BMI/js/script.js
@@ -7,34 +7,36 @@ function calculate() {
   result = weight / height ** 2;
   // Verifying if the values are null
   if (weight == 0 || height == 0) {
-    document.getElementById("result").textContent =
+    document.getElementById("result2").textContent =
       "Fill the values correctly!";
   }
   // Rouding the value to 2 decimals.
-  result = parseInt(result.toFixed(2));
+  result = result.toFixed(2);
   // Verification based on the result
   if (result < 15) {
-    document.getElementById("result").textContent =
-      "Very Severely underweight. BMI: " + result;
+    document.getElementById("result").textContent = "BMI: " + result;
+    document.getElementById("result2").textContent =
+      "Very Severely underweight";
   } else if (result > 15 && result < 16) {
-    document.getElementById("result").textContent =
-      "Severely underweight. BMI: " + result;
+    document.getElementById("result").textContent = "BMI: " + result;
+    document.getElementById("result2").textContent = "Severely underweight";
   } else if (result > 16 && result < 18.5) {
-    document.getElementById("result").textContent =
-      "Underweight. BMI: " + result;
+    document.getElementById("result").textContent = "BMI: " + result;
+    document.getElementById("result2").textContent = "Underweight";
   } else if (result > 18.5 && result < 25) {
-    document.getElementById("result").textContent = "Normal. BMI: " + result;
+    document.getElementById("result").textContent = "BMI: " + result;
+    document.getElementById("result2").textContent = "Normal";
   } else if (result > 25 && result < 30) {
-    document.getElementById("result").textContent =
-      "Overweight. BMI: " + result;
+    document.getElementById("result").textContent = "BMI: " + result;
+    document.getElementById("result2").textContent = "Overweight";
   } else if (result > 30 && result < 35) {
-    document.getElementById("result").textContent =
-      "Moderately Obese. BMI: " + result;
+    document.getElementById("result").textContent = "BMI: " + result;
+    document.getElementById("result2").textContent = "Moderately Obese";
   } else if (result > 35 && result < 40) {
-    document.getElementById("result").textContent =
-      "Severely Obese. BMI: " + result;
+    document.getElementById("result").textContent = "BMI: " + result;
+    document.getElementById("result2").textContent = "Severely Obese";
   } else if (result > 40) {
-    document.getElementById("result").textContent =
-      "Very Severely Obese. BMI: " + result;
+    document.getElementById("result").textContent = "BMI: " + result;
+    document.getElementById("result2").textContent = "Very Severely Obese";
   }
 }
