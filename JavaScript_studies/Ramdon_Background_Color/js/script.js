4c25466228bf1471312be029d29f89215a602daa
LucasBiazi <lucas.ferreira@novaandradina.org>
2021-05-06 14:11:20 -0400

    Final adjustments.

--- JavaScript_studies/Ramdon_Background_Color/js/script.js
+++ JavaScript_studies/Ramdon_Background_Color/js/script.js
@@ -4,6 +4,7 @@ Change_bg = () => {
         let ramdon2 = Math.floor(Math.random() * (255 - 0 + 1) + 0)
         let ramdon3 = Math.floor(Math.random() * (255 - 0 + 1) + 0)
         document.getElementById("body").style = "background-color: rgb( " + ramdon1 + ", " + ramdon2 + ", " + ramdon3 + ")";
+        console.log("Color scheme: rgb( " + ramdon1 + ", " + ramdon2 + ", " + ramdon3 + ")")
     })
 }
 document.addEventListener("DOMContentLoaded", () => {
