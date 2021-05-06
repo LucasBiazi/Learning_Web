c09d6289c40b284775daeaeb4c067dcaf8828d12
LucasBiazi <lucas.ferreira@novaandradina.org>
2021-05-06 12:20:28 -0400

    Script almost complete.

--- JavaScript_studies/grocery_store/js/script.js
+++ JavaScript_studies/grocery_store/js/script.js
@@ -1,9 +1,34 @@
-Clean_Selections = () => {
-    for (let i = 1; i < 8; i++) { document.getElementById("Item" + i).checked = false }
+Clean_selections = () => {
+    for (let i = 1; i < 8; i++) document.getElementById("Item" + i).checked = false
+    document.getElementById("total").innerText = 0
+}
+
+Get_values = () => {
+    const values = []
+    for (let i = 1; i < 8; i++) values[i] = parseInt(document.getElementById("table").rows[i].cells[1].innerText.split(" ").pop())
+    return values
+}
+
+
+Add_items = () => {
+    for (let i = 1; i < 8; i++) {
+        document.getElementById("Item" + i).addEventListener("click", () => {
+            let total = parseInt(document.getElementById("total").innerText)
+            if (document.getElementById("Item" + i).checked == true) {
+                total += Get_values()[i]
+                document.getElementById("total").innerText = total
+            } else
+                total -= Get_values()[i]
+            document.getElementById("total").innerText = total
+        })
+    }
+
 }
 
 // Load Page buttons
 document.addEventListener("DOMContentLoaded", () => {
     const clean_button = document.getElementById("Clean")
-    clean_button.addEventListener("click", Clean_Selections)
+    clean_button.addEventListener("click", Clean_selections)
+    Clean_selections()
+    Add_items()
 })
