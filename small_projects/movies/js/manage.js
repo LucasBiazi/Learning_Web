de5d1bf2ad46ff965c62d374bc471ab9fe094a47
LucasBiazi <lucas.ferreira@novaandradina.org>
2022-06-01 11:26:36 -0400

    Buttons' functionality.

--- small_projects/movies/js/manage.js
+++ small_projects/movies/js/manage.js
@@ -1,8 +1,33 @@
+const register_movie = () => {
+  //Create Elements
+  const main = document.getElementById("main");
+  const form = document.createElement("form");
+  const name_label = document.createElement("label");
+  const name_input = document.createElement("input");
+  const year_label = document.createElement("label");
+  const year_input = document.createElement("input");
+  const description_label = document.createElement("label");
+  const description_input = document.createElement("input");
+
+  //Tweaking Elements
+  form.classList.add("main_form");
+
+  //Appending Elements
+  main.appendChild(form);
+  form.appendChild(
+    name_label,
+    name_input,
+    year_label,
+    year_input,
+    description_label,
+    description_input
+  );
+};
+
 const load_buttons = () => {
-  const manage_movies_button = document.getElementById("manage");
+  const register_movie_button = document.getElementById("register");
   const movie_list_button = document.getElementById("list");
-  manage_movies_button.addEventListener("click");
-  movie_list_button.addEventListener("click");
+  register_movie_button.addEventListener("click", register_movie);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
