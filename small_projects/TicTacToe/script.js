eb3ec62d8808e075dfae4dc03d523005dc2affdd
LucasBiazi <lucas.ferreira@novaandradina.org>
2022-06-05 17:03:56 -0400

    Adding the greet.

--- small_projects/TicTacToe/script.js
+++ small_projects/TicTacToe/script.js
@@ -1,1 +1,27 @@
-const greet_player = () => {};
+const greet_player = () => {
+  //Create elements
+  const greet_div = document.createElement("div");
+  const how_to_play_div = document.createElement("div");
+  const how_to_play_label = document.createElement("label");
+  const how_to_play = document.createElement("p");
+  const choose_div = document.createElement("div");
+  const choose_label = document.createElement("label");
+  const div_X = document.createElement("div");
+  const div_O = document.createElement("div");
+
+  //Adding classes
+  greet_div.classList.add("greet_div");
+
+  //Appending Children
+  greet_div.appendChild(how_to_play_div);
+  greet_div.appendChild(choose_div);
+  how_to_play_div.appendChild(how_to_play_label);
+  how_to_play_div.appendChild(how_to_play);
+  choose_div.appendChild(choose_label);
+  choose_div.appendChild(div_X);
+  choose_div.appendChild(div_O);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  greet_player();
+});
