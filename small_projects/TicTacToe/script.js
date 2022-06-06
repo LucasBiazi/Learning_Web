1148efb1f6a6135ab9aade88b8038f475d161a4b
LucasBiazi <lucas.ferreira@novaandradina.org>
2022-06-06 08:13:35 -0400

    Adding the map.

--- small_projects/TicTacToe/script.js
+++ small_projects/TicTacToe/script.js
@@ -1,3 +1,8 @@
+const start_game = () => {
+  const greet_div = document.getElementById("greet_div");
+  greet_div.remove();
+};
+
 const greet_player = () => {
   //Create elements
   const body = document.getElementById("body");
@@ -7,6 +12,7 @@ const greet_player = () => {
   const how_to_play = document.createElement("p");
   const choose_div = document.createElement("div");
   const choose_label = document.createElement("label");
+  const div_buttons = document.createElement("div");
   const div_X = document.createElement("div");
   const div_O = document.createElement("div");
 
@@ -14,8 +20,11 @@ const greet_player = () => {
   greet_div.classList.add("greet_div");
   how_to_play_div.classList.add("how_to_play_div");
   choose_div.classList.add("choose_div");
-  how_to_play_label.classList.add("how_to_play_label")
-  choose_label.classList.add("choose_label")
+  how_to_play_label.classList.add("how_to_play_label");
+  choose_label.classList.add("choose_label");
+  div_buttons.classList.add("div_buttons");
+  div_O.classList.add("div_O");
+  div_X.classList.add("div_X");
 
   //Appending Children
   body.appendChild(greet_div);
@@ -24,14 +33,20 @@ const greet_player = () => {
   how_to_play_div.appendChild(how_to_play_label);
   how_to_play_div.appendChild(how_to_play);
   choose_div.appendChild(choose_label);
-  choose_div.appendChild(div_X);
-  choose_div.appendChild(div_O);
+  choose_div.appendChild(div_buttons);
+  div_buttons.appendChild(div_O);
+  div_buttons.appendChild(div_X);
 
   //Giving them values
+  greet_div.setAttribute("id", "greet_div");
   how_to_play_label.innerHTML = "How to play Tic-Tac-Toe";
   how_to_play.innerHTML =
     "To win in Tic Tac Toe you must form an uninterrupted row with the chosen symbol. You can play with either the X's or O's.";
   choose_label.innerHTML = "Choose your symbol";
+
+  //Buttons' events
+  div_O.addEventListener("click", start_game);
+  div_X.addEventListener("click", start_game);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
