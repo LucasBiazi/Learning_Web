4a0a89624b92f59e77da0bfd8476ff7db2a307c0
LucasBiazi <lucas.ferreira@novaandradina.org>
2022-06-12 14:00:32 -0400

    Player can now shoot.

--- small_projects/space_invaders_clone/script.js
+++ small_projects/space_invaders_clone/script.js
@@ -1,3 +1,24 @@
+const shoot = (x) => {
+  //Creates bullet
+  const main = document.getElementById("main");
+  const div_bullet = document.createElement("div");
+
+  //Appends child
+  main.appendChild(div_bullet);
+
+  //Sets position
+  div_bullet.style.top = "340px";
+  div_bullet.style.left = x + "px";
+
+  //Gives it a class
+  div_bullet.classList.add("div_bullet");
+  
+  //Deletes bullet
+  setTimeout(() => {
+    div_bullet.remove();
+  }, 1000);
+};
+
 const load_game = () => {
   //Adding movement to the spaceship
   const space_ship = document.getElementById("space_ship");
@@ -15,7 +36,8 @@ const load_game = () => {
         space_ship.style.left = x + "px";
         break;
       case "ArrowUp":
-          
+        if (document.getElementsByClassName("div_bullet").length === 0)
+          shoot(x + 20);
         break;
       default:
         break;
