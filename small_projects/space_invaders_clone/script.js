e7f4850a45a489e7721dbfd75e7e614a00a77dae
LucasBiazi <lucas.ferreira@novaandradina.org>
2022-06-12 16:02:35 -0400

    Collision System.

--- small_projects/space_invaders_clone/script.js
+++ small_projects/space_invaders_clone/script.js
@@ -12,7 +12,8 @@ const shoot = (x) => {
 
   //Gives it a class
   div_bullet.classList.add("div_bullet");
-  
+  div_bullet.setAttribute("id", "bullet");
+
   //Deletes bullet
   setTimeout(() => {
     div_bullet.remove();
@@ -45,6 +46,47 @@ const load_game = () => {
   });
 };
 
+const collision_system = () => {
+  setInterval(() => {
+    //Checks if any enemy was hit
+    const e1 = document.getElementById("e1");
+    const e2 = document.getElementById("e2");
+    const e3 = document.getElementById("e3");
+    const e4 = document.getElementById("e4");
+    const e5 = document.getElementById("e5");
+
+    const enemies_position = [
+      { x: e1.getBoundingClientRect().x, y: e1.getBoundingClientRect().y },
+      { x: e2.getBoundingClientRect().x, y: e2.getBoundingClientRect().y },
+      { x: e3.getBoundingClientRect().x, y: e3.getBoundingClientRect().y },
+      { x: e4.getBoundingClientRect().x, y: e4.getBoundingClientRect().y },
+      { x: e5.getBoundingClientRect().x, y: e5.getBoundingClientRect().y },
+    ];
+
+    if (document.getElementById("bullet")) {
+      const x_bullet = document
+        .getElementById("bullet")
+        .getBoundingClientRect().x;
+      const y_bullet = document
+        .getElementById("bullet")
+        .getBoundingClientRect().y;
+
+      console.log(
+        "X: " + enemies_position[0].x + "Y: " + enemies_position[0].y
+      );
+      console.log("X bullet: " + x_bullet + "Y bullet : " + y_bullet);
+      for (let i = 0; i < 5; i++) {
+        if (
+          enemies_position[i].x === x_bullet &&
+          enemies_position[i].y === y_bullet
+        )
+          alert("Shoot!");
+      }
+    }
+  }, 10);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   load_game();
+  collision_system();
 });
